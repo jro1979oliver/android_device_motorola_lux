e3e2c6d4b08b4f5fb4982fede9a638441c6dc6a6
Jeferson <jroliveira.oliveira301@gmail.com>
2021-04-20 07:49:06 -0700

    lux: Update dalvik.vm.heapgrowthlimit to 256m
    Change-Id: I64bf069559d8956706da731cdb058404c5805417

--- system_prop.mk
+++ system_prop.mk
@@ -17,8 +17,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapstartsize=24m \
+    dalvik.vm.heapgrowthlimit=256m \
     dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=2m \
